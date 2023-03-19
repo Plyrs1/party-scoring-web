6d147c7221b18052449e11aec47426810e2f7262
ccrsxx <aminrisal@gmail.com>
2023-03-19 11:05:10 +0700

    feat: implement basic layout (#15)
    * build: fix renovate schedule (#10)
    
    * build: add renovate automerge
    
    * build: fix renovate schedule
    
    * chore: add svelte-fa and its icons
    
    * refactor: move body class style to main.css
    
    * feat: add project design colors
    
    * feat: implement the basic look of the homepage
    
    * chore: sync yarn.lock with package.json
    
    * fix: avoid using reserve class
    
    * feat: add missing routes
    
    * feat: finish implementing basic layout
    
    ---------
    
    Co-authored-by: Plyrs1 <me@plyrs.eu.org>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
-<p>Party Scoring</p>
-<a href="/home">Home</a>
-<a href="/songlist">Song List</a>
-<a href="/userlist">User List</a>
+<script>
+  import Fa from 'svelte-fa'
+
+  import { faAnglesRight } from '@fortawesome/free-solid-svg-icons'
+</script>
+
+<main class="rounded-xl grid gap-6 grid-rows-[auto,1fr] w-full">
+  <section class="widget px-6 py-4">
+    <h1 class="flex items-center gap-4 text-xl">
+      Home <Fa class="text-party-orange" icon={faAnglesRight} />
+      Home <Fa class="text-party-orange" icon={faAnglesRight} />
+      Home
+    </h1>
+  </section>
+  <section class="widget" />
+</main>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,1 @@
+<p>This is profile page</p>
