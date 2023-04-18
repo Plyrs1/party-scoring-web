6d6fa41c4ab912c715c91fe688f2c28d7f25a4d4
ccrsxx <aminrisal@gmail.com>
2023-04-18 12:22:56 +0700

    feat: implement basic video ui

--- src/routes/songlist/+page.svelte
+++ src/routes/songlist/+page.svelte
@@ -1,1 +1,12 @@
-<p>This is songlist page</p>
+<script lang="ts">
+  import VideoPlayer from '$lib/components/VideoPlayer.svelte'
+</script>
+
+<main class="rounded-xl grid gap-6">
+  <VideoPlayer 
+    title="Yotogibanashi" 
+    artist="KanoeRana" 
+    subtitle="Kyokou Suiri Season 2 - OP1" 
+    songLink="#" 
+  />
+</main>
