0f9b123277271104f1bdaf25f4b9d97f8e80aa8f
ccrsxx <aminrisal@gmail.com>
2023-04-28 00:53:59 +0700

    feat: make color the same as the design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       //   },
       colors: {
         'party-background': '#D9D9D9',
-        'party-orange': '#EAA133'
+        'party-orange': '#EAA033'
         // 'kusogaki-purple': '#6167c5',
         // 'kusogaki-blue': '#91a8dd',
         // 'kusogaki-red': '#eba3b4',
